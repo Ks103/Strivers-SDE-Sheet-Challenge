40475dd875b32308cb7917b0341ddd729e929357
Khushi Shroff <86292101+Ks103@users.noreply.github.com>
2023-07-05 11:32:19 +0530

    Create Celebrity problem.cpp

---
+++ DAY 14/Celebrity problem.cpp
@@ -0,0 +1,14 @@
+int findCelebrity(int n) {
+ 	// Write your code here.
+    int c=0;
+    for(int i=1;i<n;i++){
+        if(knows(c,i)==1){
+            c=i;
+        }
+     }
+    for(int i=0;i<n;i++){
+        if(i!=c and (knows(c,i)==1 or knows(i,c)==0))
+            return -1;
+       }
+   return c;
+}
